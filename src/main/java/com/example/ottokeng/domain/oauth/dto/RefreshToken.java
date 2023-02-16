c74dbf62bab8fcc45fe54a8b9ab985b874465e30
jangwooooo <s22018@gsm.hs.kr>
2023-02-16 20:14:49 +0900

    Add refreshToken클래스 구현

---
+++ src/main/java/com/example/ottokeng/domain/oauth/dto/RefreshToken.java
@@ -0,0 +1,32 @@
+package com.example.ottokeng.domain.oauth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@RedisHash("refreshToken")
+public class RefreshToken {
+
+    @Id
+    private String oauthId;
+
+    @Indexed
+    private String refreshToken;
+
+    @TimeToLive
+    private long expiredAt;
+
+    public void exchangeRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+}
