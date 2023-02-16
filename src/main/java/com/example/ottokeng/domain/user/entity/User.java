f2c920881c7006d90b08ff8a102c77c28ac94ea0
jangwooooo <s22018@gsm.hs.kr>
2023-02-16 18:51:29 +0900

    Add User entity

---
+++ src/main/java/com/example/ottokeng/domain/user/entity/User.java
@@ -0,0 +1,36 @@
+package com.example.ottokeng.domain.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String imageUrl;
+
+    private String oauthId;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public User update(String name, String imageUrl) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+        return this;
+    }
+}
+
