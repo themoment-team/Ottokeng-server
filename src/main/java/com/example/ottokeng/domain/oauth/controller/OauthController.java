9f0cf2a1ee40e793fd910b6adf4f22b112baf44a
jangwooooo <s22018@gsm.hs.kr>
2023-02-16 20:01:53 +0900

    Add OauthController 구현

---
+++ src/main/java/com/example/ottokeng/domain/oauth/controller/OauthController.java
@@ -0,0 +1,21 @@
+package com.example.ottokeng.domain.oauth.controller;
+
+import com.example.ottokeng.domain.oauth.dto.LoginResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class OauthController {
+
+    private final OauthService oauthService;
+
+    @GetMapping("/login/oauth/{provider}")
+    public ResponseEntity<LoginResponse> login(@PathVariable String provider, @RequestParam String code) {
+        LoginResponse loginResponse = oauthService.login(provider, code);
+        return new ResponseEntity<>(loginResponse, HttpStatus.OK);
+    }
+
+}
