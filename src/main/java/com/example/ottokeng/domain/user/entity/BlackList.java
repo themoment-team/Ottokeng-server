651e0c4f18077f31f33c60973947a7a60a95ffc7
jangwooooo <s22018@gsm.hs.kr>
2023-02-19 09:01:01 +0900

    Implement BlackList entity

---
+++ src/main/java/com/example/ottokeng/domain/user/entity/BlackList.java
@@ -0,0 +1,20 @@
+package com.example.ottokeng.domain.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@RedisHash()
+public class BlackList {
+
+    @Id
+    private String accessToken;
+    private String oauthId;
+}
