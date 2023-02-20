a9b4f87aeff095681173a4150ba6a4d8a473474f
hajeu <s22072@gsm.hs.kr>
2023-02-20 22:37:02 +0900

    controller 수정

--- src/main/java/com/example/ottokeng/domain/find/presentation/FindController.java
+++ src/main/java/com/example/ottokeng/domain/find/presentation/FindController.java
@@ -1,6 +1,7 @@
 package com.example.ottokeng.domain.find.presentation;
 
 import com.example.ottokeng.domain.find.presentation.dto.response.ShowFindResponse;
+import com.example.ottokeng.domain.find.presentation.dto.response.ShowFindsResponse;
 import com.example.ottokeng.domain.find.service.FindWritingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,8 +18,8 @@ public class FindController {
     private final FindWritingService findWritingService;
 
     @GetMapping("/showfind")
-    public ResponseEntity<ShowFindResponse> showfind(){
-        ShowFindResponse showfindResponse = findWritingService.getFind();
-        return new ResponseEntity<>(showfindResponse, HttpStatus.OK);
+    public ResponseEntity<ShowFindsResponse> showfind(){
+        ShowFindsResponse showfindsResponse = findWritingService.getFind();
+        return new ResponseEntity<>(showfindsResponse, HttpStatus.OK);
     }
 }
