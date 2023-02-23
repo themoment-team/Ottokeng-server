46317325e3b9b051e415920a3d4ecd3cf8ec6e53
hajeu <s22072@gsm.hs.kr>
2023-02-23 17:53:28 +0900

    mapping

--- src/main/java/com/example/ottokeng/domain/user/entity/User.java
+++ src/main/java/com/example/ottokeng/domain/user/entity/User.java
@@ -1,5 +1,7 @@
 package com.example.ottokeng.domain.user.entity;
 
+import com.example.ottokeng.domain.find.entity.Find;
+import com.example.ottokeng.domain.lost.entity.Lost;
 import com.example.ottokeng.global.enumType.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,6 +29,14 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @OneToOne
+    @JoinColumn(name = "find_id")
+    private Find find;
+
+    @OneToOne
+    @JoinColumn(name = "lost_id")
+    private Lost lost;
 
     public User update(String name, String imageUrl) {
         this.name = name;
