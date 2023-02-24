a96d57aa9a7f81dec939518fcc2df53e19d79e31
hajeu <s22072@gsm.hs.kr>
2023-02-24 12:06:30 +0900

    test

--- src/main/java/com/example/ottokeng/global/exception/ErrorCode.java
+++ src/main/java/com/example/ottokeng/global/exception/ErrorCode.java
@@ -12,7 +12,9 @@ public enum ErrorCode {
     INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
     USER_NOT_FOUND(404, "존재하지 않는 유저입니다."),
     UNABLE_TO_ISSUANCE_REFRESHTOKEN(400, "유효하지 않은 리프레쉬 토큰입니다."),
-    ALREADY_BLACKLIST(409, "이미 블랙리스트에 존재합니다.");
+    ALREADY_BLACKLIST(409, "이미 블랙리스트에 존재합니다."),
+    FIND_WRITING_NOT_FOUND(404, "존재하지 않는 찾기 글입니다."),
+    LOST_WRITING_NOT_FOUND(404, "존재하지 않는 등록 글입니다.");;
 
     private final int status;
     private final String detail;
