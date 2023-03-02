3ed6082365c12624dd0ba73af37110ce0a8e264f
jangwooooo <s22018@gsm.hs.kr>
2023-03-03 08:31:47 +0900

    Add Image엔티티

---
+++ src/main/java/com/example/ottokeng/domain/post/entity/Image.java
@@ -0,0 +1,26 @@
+package com.example.ottokeng.domain.post.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
