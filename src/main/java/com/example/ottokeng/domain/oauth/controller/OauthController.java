4953650021292024f523b0921924243429a06952
jangwooooo <s22018@gsm.hs.kr>
2023-03-23 20:28:25 +0900

    Add OauthController Crossorigin

--- src/main/java/com/example/ottokeng/domain/oauth/controller/OauthController.java
+++ src/main/java/com/example/ottokeng/domain/oauth/controller/OauthController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin
 public class OauthController {
 
     private final OauthService oauthService;
