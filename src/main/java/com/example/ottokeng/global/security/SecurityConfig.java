5abe9e80f89f53b1a1c7a260ddfe9a94f96dbc80
hajeu <s22072@gsm.hs.kr>
2023-03-29 23:25:20 +0900

    sessionManagement() 추가

--- src/main/java/com/example/ottokeng/global/security/SecurityConfig.java
+++ src/main/java/com/example/ottokeng/global/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -32,12 +33,12 @@ public class SecurityConfig{
                 .antMatchers("/login/oauth/**").permitAll()
                 .antMatchers("/token/reissue").permitAll()
                 .antMatchers(HttpMethod.GET, "/post/comment/**").permitAll()
-                .antMatchers( HttpMethod.GET,"/post/writing").permitAll()
+                .antMatchers(HttpMethod.GET,"/post/writing").permitAll()
                 .antMatchers("/my-page/**").hasAuthority("ROLE_USER")
-                .antMatchers("/").permitAll();
-
-        http.authorizeRequests()
                 .anyRequest().authenticated();
+
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
         http.addFilterBefore(jwtExceptionFilter, JwtTokenFilter.class);
