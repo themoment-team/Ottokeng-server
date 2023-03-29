f9a0622ff3c409c8de6bd314a94e25fb2300d8cf
hajeu <s22072@gsm.hs.kr>
2023-03-29 19:58:14 +0900

    WebConfig allowedOrigins 도메인 주소 슬래시 제거

--- src/main/java/com/example/ottokeng/global/config/WebConfig.java
+++ src/main/java/com/example/ottokeng/global/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("https://server.ottokeng.site/","https://www.ottokeng.site/","https://ottokeng.site/","http://server.ottokeng.site/","http://www.ottokeng.site/","http://ottokeng.site/","http://localhost:3000")
+                .allowedOrigins("https://server.ottokeng.site","https://www.ottokeng.site","https://ottokeng.site","http://server.ottokeng.site","http://www.ottokeng.site","http://ottokeng.site","http://localhost:3000")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
